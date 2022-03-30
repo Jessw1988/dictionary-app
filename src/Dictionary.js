71c2567c78ac9c75789e9f4dccc79b6d189a936e
Jessw1988 <93623599+Jessw1988@users.noreply.github.com>
2022-03-31 10:00:04 +1100

    fixed error
    fixed error

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,20 +5,19 @@ import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState({});
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_us/${keyword}`;
-    console.log(apiUrl);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }

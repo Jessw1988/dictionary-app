3cc3d9ade8a7a288fb451c19821ce0168a251f70
Jessw1988 <93623599+Jessw1988@users.noreply.github.com>
2022-03-31 14:07:40 +1100

    fixed more errors
    fixed more errors

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,7 +3,6 @@ import Synonyms from "./Synonyms";
 import "./Meaning.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3> {props.meaning.partOfSpeech} </h3>
@@ -11,17 +10,14 @@ export default function Meaning(props) {
         if (index === 0) {
           return (
             <div key={index}>
-              <p>
-                <div className="defintion">{definition.definition}</div>
-                <br />
-                <div className="example">{definition.example}</div>
-              </p>
+              <div className="defintion">{definition.definition}</div>
+              <br />
+              <div className="example">{definition.example}</div>
+              <Synonyms synonyms={props.meaning.synonyms} />
             </div>
           );
         }
       })}
-
-      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,9 +4,7 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
-        Listen
-      </a>
+      <a href={props.phonetic.audio}>Listen</a>
       <span className="text mt-2"> {props.phonetic.text} </span>
     </div>
   );
